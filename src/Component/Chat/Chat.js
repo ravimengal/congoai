822cc243ffcde466ecbf27a1306203672ebf7251
ravimengal <mengal22ravi@gmail.com>
2021-12-22 18:19:58 +0530

    added css

--- src/Component/Chat/Chat.js
+++ src/Component/Chat/Chat.js
@@ -1,36 +1,32 @@
-import React from 'react'
-import "./Chat.css"
+import React from "react";
+import "./Chat.css";
 
 const Chat = () => {
-    return (
-        <div>
-            <div className="Chat_container">
-            <div className='Chat_wrapper'>
-           <div className='Chat_header'>
-               <h1>IRIS</h1>
-               <h1>Hello </h1>
-               <h1>I am Iris, a virtual Assistant</h1>
-               <h1>How may i help you?</h1>
-               <div className='Live_Chat_wrapper'>
-
-                   <ui>
-                    
-                   <li>Can i redeem my FB before the original term?</li>
-                   <li>How do i pa my Credit card bill? </li>
-                   <li>How can i get my account Statement? </li>
-                      <li>What is the tenture Deposit? </li>
-                         
-                   </ui>
-                   <div className='btn'>
-                   <button> Start a New Conversation</button>
-                    </div>
-
-               </div>
-           </div>
+  return (
+    <div>
+      <div className="Chat_container">
+        <div className="Chat_wrapper">
+          <div className="Chat_header">
+            <h1>IRIS</h1>
+            <h1>Hello </h1>
+            <h1>I am Iris, a virtual Assistant</h1>
+            <h1>How may i help you?</h1>
+            <div className="Live_Chat_wrapper">
+              <ui>
+                <li>Can i redeem my FB before the original term?</li>
+                <li>How do i pa my Credit card bill? </li>
+                <li>How can i get my account Statement? </li>
+                <li>What is the tenture Deposit? </li>
+              </ui>
+              <div className="btn">
+                <button> Start a New Conversation</button>
+              </div>
+            </div>
+          </div>
         </div>
-        </div>
-        </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Chat
+export default Chat;
